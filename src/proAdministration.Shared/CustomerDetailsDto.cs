3999d04ecb05e1560062f8128dcf18d8c4f558df
Mark Novak <mark@m8it.io>
2024-06-25 16:09:33 +0200

    feat(api): add data transfer objects

---
+++ src/proAdministration.Shared/CustomerDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace proAdministration.Shared;
+
+public class CustomerDetailsDto
+{
+    // TODO: Evaluate if separate address fields are preferable 
+    public int Id { get; set; }
+    public string CustomerId { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Currency { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+}
---
+++ src/proAdministration.Shared/CustomerRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace proAdministration.Shared;
+
+public record CustomerRequest(
+    string CustomerId,
+    string Name,
+    string Email,
+    string PhoneNumber,
+    string Currency,
+    string Street,
+    string State,
+    string City,
+    string PostalCode,
+    string Country);
