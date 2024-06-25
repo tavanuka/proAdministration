1eb657d089ebbff77cd4e40a0b5f7699c83baef2
Mark Novak <mark@m8it.io>
2024-06-26 01:58:15 +0200

    feat(client): add initial form

--- src/proadministration-client/app/desktop/src/view/customers/CustomersView.js
+++ src/proadministration-client/app/desktop/src/view/customers/CustomersView.js
@@ -2,13 +2,74 @@ Ext.define('proAdministration.Client.view.customers.CustomersView', {
     extend: 'Ext.grid.Grid',
     xtype: 'customersview',
     cls: 'customersview',
-    // controller: { type: 'customersviewcontroller' },
+    controller: { type: 'customersviewcontroller' },
     // viewModel: { type: 'customersviewmodel' },
     store: { type: 'customersviewstore' },
+    requires: [
+        'Ext.Dialog',
+        'Ext.form.Panel'
+    ],
+
+    dialog: {
+        xtype: 'dialog',
+        title: 'Dialog',
+
+        closable: true,
+        defaultFocus: 'textfield',
+        maskTapHandler: 'onCancel',
+
+        bodyPadding: 0,
+        layout: 'fit',
+        maxWidth: 550,
+
+        items: [{
+            xtype: 'formpanel',
+            reference: 'form',
+            autoSize: true,
+            items: [{
+                xtype: 'textfield',
+                label: 'Name',
+                required: true,
+                bind: '{name}',
+                allowBlank: false
+            }, {
+                xtype: 'emailfield',
+                name: 'email',
+                label: 'Email',
+                bind: '{email}',
+                allowBlank: false,
+                required: true,
+                validators: 'email'
+            }]
+        }],
+
+        // We are using standard buttons on the button
+        // toolbar, so their text and order are consistent.
+        buttons: {
+            ok: 'onOk',
+            cancel: 'onCancel'
+        }
+    },
 
     columns: [
         { text: 'Name', dataIndex: 'name', flex: 1 },
         { text: 'Email', dataIndex: 'email', flex: 1 },
-        { text: 'Phone', dataIndex: 'phoneNumber', flex: 1 }
+        { text: 'Phone', dataIndex: 'phoneNumber', flex: 1 },
+        { text: 'Currency', dataIndex: 'currency', flex: 1 },
+        { text: 'Address', dataIndex: 'address', flex: 1 },
+        { text: 'Country', dataIndex: 'country', flex: 1 },
+        {
+            width: 50,
+            hideable: false,
+
+            cell: {
+                tools: {
+                    approve: {
+                        iconCls: 'x-fa fa-check green',
+                        handler: 'showDialog'
+                    }
+                }
+            }
+        }
     ]
 });
--- src/proadministration-client/app/desktop/src/view/customers/CustomersViewController.js
+++ src/proadministration-client/app/desktop/src/view/customers/CustomersViewController.js
@@ -1,4 +1,54 @@
 Ext.define('proAdministration.Client.view.customersCustomersViewController', {
     extend: 'Ext.app.ViewController',
-    alias: 'controller.customersviewcontroller'
+    alias: 'controller.customersviewcontroller',
+
+    destroy: function () {
+        Ext.destroy(this.dialog);
+
+        this.callParent();
+    },
+
+    hideDialog: function () {
+        var dialog = this.dialog;
+
+        if (dialog) {
+            dialog.hide();
+        }
+    },
+
+    showDialog: function () {
+        var dialog = this.dialog,
+            view;
+
+        if (!dialog) {
+            view = this.getView();
+            dialog = Ext.apply({
+                hideMode: 'offsets',
+                ownerCmp: view
+            }, view.dialog);
+
+            this.dialog = dialog = Ext.create(dialog);
+        }
+
+        dialog.show();
+    },
+
+    onCancel: function () {
+        this.hideDialog();
+    },
+
+    onOk: function () {
+        var form = this.lookup('form');
+
+        if (form.validate()) {
+            // In a real application, this would submit the form to the configured url
+            // form.submit();
+            this.hideDialog();
+
+            Ext.Msg.alert(
+                'Thank you!',
+                'Your inquiry has been sent. We will respond as soon as possible.'
+            );
+        }
+    }
 });
